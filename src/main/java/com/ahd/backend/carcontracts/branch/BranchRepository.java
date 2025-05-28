b84a118d5ca867d608c1e436dd6aadee5d917b24
Ahmed Rahman Hassani <ah.jr22@gmail.com>
2025-05-28 23:13:26 +0300

    Delete src/main/java/com/ahd/backend/carcontracts/branch directory

--- src/main/java/com/ahd/backend/carcontracts/branch/BranchRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ahd.backend.carcontracts.branch;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BranchRepository extends JpaRepository<Branch, Long> {
-}
