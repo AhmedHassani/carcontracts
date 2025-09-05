c0c5fd66f7cb5c178abf52de26f5d6b5863a3ca5
ahmedrhaman <ah.jr22@gmail.com>
2025-09-05 22:07:02 +0300

    Helper.java

--- src/main/java/com/ahd/backend/carcontracts/util/Helper.java
+++ src/main/java/com/ahd/backend/carcontracts/util/Helper.java
@@ -3,6 +3,8 @@ package com.ahd.backend.carcontracts.util;
 
 import com.ahd.backend.carcontracts.appuser.models.AppUser;
 import com.ahd.backend.carcontracts.appuser.repository.UserRepository;
+import com.ahd.backend.carcontracts.company.model.CompanyUser;
+import com.ahd.backend.carcontracts.company.repository.CompanyUserRepository;
 import com.ahd.backend.carcontracts.exception.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
@@ -15,10 +17,18 @@ public class Helper {
 
     private final UserRepository appUserRepository;
 
+    private final CompanyUserRepository companyUserRepository;
+
     public AppUser getCurrentUser() {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         return appUserRepository.findByUsername(auth.getName())
                 .orElseThrow(() -> new ResourceNotFoundException("Current user not found"));
+    }
+
+    public long getCurrentCompanyId(){
+        long userId = this.getCurrentUser().getId();
+        CompanyUser companyUser = companyUserRepository.findByUserId(userId);
+        return companyUser.getCompany().getId();
     }
 
 }
