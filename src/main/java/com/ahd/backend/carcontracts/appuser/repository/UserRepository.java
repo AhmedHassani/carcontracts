64763be4417caae78033ddd983741b9ef2e83cb4
ali.safaa <ali.safaa@ALSA-BAS>
2025-09-13 15:44:01 +0300

    audit logs show by each company

--- src/main/java/com/ahd/backend/carcontracts/appuser/repository/UserRepository.java
+++ src/main/java/com/ahd/backend/carcontracts/appuser/repository/UserRepository.java
@@ -5,7 +5,9 @@ import java.util.Optional;
 
 import com.ahd.backend.carcontracts.appuser.models.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<AppUser, Long> {
     Optional<AppUser> findByUsername(String username);
 }
